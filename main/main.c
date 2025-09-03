93068dbb1395d016cd5fe50d0b2c04deb27fcc50
Jmscoot <1002seok@naver.com>
2025-09-03 15:41:20 +0900

    1

--- main/main.c
+++ main/main.c
@@ -17,18 +17,21 @@
 
 static adc_channel_t channel[1] = {ADC_CHANNEL_7};
 
-static TaskHandle_t s_task_handle;
+static TaskHandle_t adc_task_handle = NULL;
 static const char *TAG = "ADC";
+uint32_t ret_num = 0;
+uint8_t result[READ_LEN] = {0};
 
 float adc_to_voltage(uint32_t adc_val) {
   return (adc_val * 3.3) / 4095.0; // 2^12=4096, 12bits
 }
+
 static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle,
                                      const adc_continuous_evt_data_t *edata,
                                      void *user_data) {
   BaseType_t mustYield = pdFALSE;
   // Notify that ADC continuous driver has done enough number of conversions
-  vTaskNotifyGiveFromISR(s_task_handle, &mustYield);
+  vTaskNotifyGiveFromISR(adc_task_handle, &mustYield);
 
   return (mustYield == pdTRUE);
 }
@@ -38,7 +41,7 @@ static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num,
   adc_continuous_handle_t handle = NULL;
 
   adc_continuous_handle_cfg_t adc_config = {
-      .max_store_buf_size = 1024,
+      .max_store_buf_size = 1024, // 512
       .conv_frame_size = READ_LEN,
   };
   ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config, &handle));
@@ -52,6 +55,7 @@ static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num,
 
   adc_digi_pattern_config_t adc_pattern[SOC_ADC_PATT_LEN_MAX] = {0};
   dig_cfg.pattern_num = channel_num;
+
   for (int i = 0; i < channel_num; i++) {
     adc_pattern[i].atten = ADC_ATTEN_DB_12;
     adc_pattern[i].channel = channel[i] & 0x7;
@@ -70,17 +74,50 @@ static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num,
 }
 
 void adc_task(void *param) {
+  adc_task_handle = xTaskGetCurrentTaskHandle();
+
+  esp_err_t ret;
+  adc_continuous_handle_t handle = (adc_continuous_handle_t)param;
+
   while (1) {
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+    ret = adc_continuous_read(handle, result, READ_LEN, &ret_num, 0);
+
+    float avg_vol = 0;
+    if (ret == ESP_OK) {
+      ESP_LOGI("TASK", "ret is %x, ret_num is %" PRIu32 " bytes", ret, ret_num);
+      for (int i = 0; i < ret_num; i += SOC_ADC_DIGI_RESULT_BYTES) {
+
+        // sample하나당 16bit(2bytes) 12(data)+4(ch)
+        adc_digi_output_data_t *p = (adc_digi_output_data_t *)&result[i];
+        uint32_t chan_num = GET_CHANNEL(p);
+        uint32_t data = GET_DATA(p);
+        float voltage = adc_to_voltage(data);
+
+        avg_vol += voltage;
+      }
+      avg_vol = avg_vol / 32;
+      if (avg_vol < 0.3) {
+        ESP_LOGI(TAG, "0");
+      } else if (avg_vol >= 0.3 && avg_vol < 0.4) {
+        ESP_LOGI(TAG, "1");
+      } else if (avg_vol >= 0.4 && avg_vol < 0.5) {
+        ESP_LOGI(TAG, "2");
+      } else if (avg_vol >= 0.5 && avg_vol < 0.7) {
+        ESP_LOGI(TAG, "3");
+      } else if (avg_vol >= 0.7) {
+        ESP_LOGI(TAG, "4");
+      }
+
+      vTaskDelay(1);
+    } else if (ret == ESP_ERR_TIMEOUT) {
+      ESP_LOGI("adc_task", "no data");
+    }
   }
 }
 
 void app_main(void) {
   esp_err_t ret;
-  uint32_t ret_num = 0;
-  uint8_t result[READ_LEN] = {0};
-  memset(result, 0xcc, READ_LEN);
-
-  s_task_handle = xTaskGetCurrentTaskHandle();
 
   adc_continuous_handle_t handle = NULL;
   continuous_adc_init(channel, sizeof(channel) / sizeof(adc_channel_t),
@@ -89,87 +126,9 @@ void app_main(void) {
   adc_continuous_evt_cbs_t cbs = {
       .on_conv_done = s_conv_done_cb,
   };
+
   ESP_ERROR_CHECK(adc_continuous_register_event_callbacks(handle, &cbs, NULL));
   ESP_ERROR_CHECK(adc_continuous_start(handle));
 
-  //   bool calibrated = adc_calibration_init();
-
-  while (1) {
-
-    /**
-     * This is to show you the way to use the ADC continuous mode driver
-     event
-     * callback. This `ulTaskNotifyTake` will block when the data processing
-     in
-     * the task is fast. However in this example, the data processing (print)
-     is
-     * slow, so you barely block here.
-     *
-     * Without using this event callback (to notify this task), you can still
-     * just call `adc_continuous_read()` here in a loop, with/without a
-     certain
-     * block timeout.
-     */
-    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-
-    // char unit[] = EXAMPLE_ADC_UNIT_STR(ADC_UNIT_1);
-    float avg_vol = 0;
-    while (1) {
-      ret = adc_continuous_read(handle, result, READ_LEN, &ret_num, 0);
-      if (ret == ESP_OK) {
-        ESP_LOGI("TASK", "ret is %x, ret_num is %" PRIu32 " bytes", ret,
-                 ret_num);
-        for (int i = 0; i < ret_num; i += SOC_ADC_DIGI_RESULT_BYTES) {
-
-          // sample하나당 16bit(2bytes) 12(data)+4(ch)
-          adc_digi_output_data_t *p = (adc_digi_output_data_t *)&result[i];
-          uint32_t chan_num = GET_CHANNEL(p);
-          uint32_t data = GET_DATA(p);
-          float voltage = adc_to_voltage(data);
-          /* Check the channel number validation, the data is invalid if the
-           * channel num exceed the maximum channel */
-          //   if (chan_num < SOC_ADC_CHANNEL_NUM(ADC_UNIT_1)) {
-          //     ESP_LOGI(TAG,
-          //              "Unit: %s, Channel: %" PRIu32 ", Value: %" PRIu32
-          //              ", Voltage: %.3fV",
-          //              unit, chan_num, data, voltage);
-          //   } else {
-          //     ESP_LOGW(TAG, "Invalid data [%s_%" PRIu32 "_%" PRIu32 "]",
-          //     unit,
-          //              chan_num, data);
-          //   }
-          avg_vol += voltage;
-        }
-        avg_vol = avg_vol / 32;
-        if (avg_vol < 0.1) {
-          ESP_LOGI(TAG, "0");
-        } else if (avg_vol >= 0.1 && avg_vol < 0.3) {
-          ESP_LOGI(TAG, "1");
-        } else if (avg_vol >= 0.3 && avg_vol < 0.5) {
-          ESP_LOGI(TAG, "2");
-        } else if (avg_vol >= 0.5 && avg_vol < 0.6) {
-          ESP_LOGI(TAG, "3");
-        } else if (avg_vol >= 0.6) {
-          ESP_LOGI(TAG, "4");
-        }
-        /**
-         * Because printing is slow, so every time you call
-         `ulTaskNotifyTake`,
-         * it will immediately return. To avoid a task watchdog timeout, add
-         a
-         * delay here. When you replace the way you process the data,
-         usually
-         * you don't need this delay (as this task will block for a while).
-         */
-        vTaskDelay(1);
-      } else if (ret == ESP_ERR_TIMEOUT) {
-        // We try to read `EXAMPLE_READ_LEN` until API returns timeout, which
-        // means there's no available data
-        break;
-      }
-    }
-  }
-
-  ESP_ERROR_CHECK(adc_continuous_stop(handle));
-  ESP_ERROR_CHECK(adc_continuous_deinit(handle));
+  xTaskCreatePinnedToCore(adc_task, "adc_task", 4096, handle, 10, NULL, 0);
 }
